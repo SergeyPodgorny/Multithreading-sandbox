ffba175ab405301227d9a3552c7bd3e5e9f5cdbb
SergeyPodgorny <serkonpod@gmail.com>
2022-02-10 14:49:13 +0300

    created some packages and classes

--- src/com/multithreading/sandbox/Sandbox.java
+++ src/com/multithreading/sandbox/Sandbox.java
@@ -5,6 +5,11 @@ public class Sandbox {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 	}
 
+	
+
+	
+	
 }
---
+++ src/com/multithreading/simpe/stuff/ThreadThing.java
@@ -0,0 +1,13 @@
+package com.multithreading.simpe.stuff;
+
+public class ThreadThing extends Thread{
+
+	
+	@Override
+	public void run() {
+		
+	}
+	
+	
+	
+}
