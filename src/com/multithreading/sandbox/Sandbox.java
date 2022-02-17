bee9e09d9f42b69c7249d60be50f01268eda805d
SergeyPodgorny <serkonpod@gmail.com>
2022-02-17 14:27:39 +0300

    started learning multithreading

--- src/com/multithreading/sandbox/Sandbox.java
+++ src/com/multithreading/sandbox/Sandbox.java
@@ -1,10 +1,25 @@
 package com.multithreading.sandbox;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Iterator;
+
+import com.multithreading.sandbox.arrays.ArraysFilling;
+
 public class Sandbox {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		
+		for (int i = 0; i < 3; i++) {
+			
+			ArraysFilling threadExample = new ArraysFilling(i);
+			
+			threadExample.start();
 
+		}
+
+		
+		
 		
 	}
 
---
+++ src/com/multithreading/sandbox/arrays/ArraysFilling.java
@@ -0,0 +1,38 @@
+package com.multithreading.sandbox.arrays;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class ArraysFilling extends Thread{
+
+	
+	private int threadNumber;
+	
+	
+	private int [] someArray = new int[10];
+		
+	public ArraysFilling(int threadNumber) {
+		this.threadNumber = threadNumber;
+		
+	}
+	
+		
+	
+	@Override
+	public void run() {
+		
+//		Instant startTime = Instant.now();	
+		for (int i = 0; i < 10; i++) {
+			
+			someArray[i] = i;
+			
+			
+			
+			System.out.println(i + " " + "from thread " + " " +threadNumber);
+
+		}
+		
+	}
+	
+
+}
