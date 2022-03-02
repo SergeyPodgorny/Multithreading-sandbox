866a0ac595cc23322883a0df21acd8ba80fdfc8e
SergeyPodgorny <serkonpod@gmail.com>
2022-03-02 14:18:03 +0300

    refactoring

--- src/com/multithreading/sandbox/thread/and/runnable/InnerClass.java
+++ src/com/multithreading/sandbox/thread/and/runnable/InnerClass.java
@@ -36,7 +36,7 @@ public class InnerClass{
 		for (int i = 0; i < matrixSize*matrixSize; i++) {
 				for (int j = 0; j < matrixSize*matrixSize; j++) {
 					if (i==j) {
-						someArray[i][j] = 1;
+						someArray[i][j] = 2;
 					}
 				}
 			}
@@ -49,11 +49,11 @@ public class InnerClass{
 		
 		@Override
 		public void run() {
-			for (int i = matrixSize; i < matrixSize*matrixSize-matrixSize; i++) {
-				for (int j = matrixSize; j <matrixSize*matrixSize-matrixSize; j++) {
+			for (int i = matrixSize+1; i < matrixSize*matrixSize-matrixSize-1; i++) {
+				for (int j = matrixSize+1; j <matrixSize*matrixSize-matrixSize-1; j++) {
 					if (i==j) {
 						someArray[i][j] = 8;
-						if ((i % matrixSize == 0)^(i % matrixSize == 1)) {
+						if ((i % matrixSize-1 == 0)^(i % matrixSize-1 == 1)) {
 							someArray[i][j] = 1;
 						}
 					}
@@ -74,8 +74,8 @@ public class InnerClass{
 		
 		@Override
 		public void run() {
-			for (int i = matrixSize + 1; i < matrixSize*matrixSize-matrixSize; i++) {
-				for (int j = matrixSize + 1; j < matrixSize*matrixSize-matrixSize; j++) {
+			for (int i = matrixSize; i < matrixSize*matrixSize-matrixSize; i++) {
+				for (int j = matrixSize; j < matrixSize*matrixSize-matrixSize; j++) {
 					if (i + 1 == j) {
 						someArray[i][j] = 3;
 					}
