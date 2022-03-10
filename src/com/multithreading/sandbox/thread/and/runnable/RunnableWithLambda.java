dda32600ffc2af4a5832f4d6fd5da4268a00ad0f
SergeyPodgorny <serkonpod@gmail.com>
2022-03-10 14:27:56 +0300

    added lambda implementation

--- src/com/multithreading/sandbox/thread/and/runnable/RunnableWithLambda.java
+++ src/com/multithreading/sandbox/thread/and/runnable/RunnableWithLambda.java
@@ -7,17 +7,114 @@ public class RunnableWithLambda extends Thread{
 	
 	private volatile int[][] someArray = new int[matrixSize*matrixSize][matrixSize*matrixSize];
 
+	
 	{
 	
-		
-		
-		
+			
 	Runnable r1 = () -> {
-		System.out.println("lambdas kik azz");
+		// sparse matrix creation
+		for (int i = 0; i < matrixSize*matrixSize; i++) {
+			for (int j = 0; j < matrixSize*matrixSize; j++) {
+				if (i==j) {
+					someArray[i][j] = 2;
+				}
+			}
+		}
 	};
 	
 	Thread t1 = new Thread(r1);
 	t1.start();
+	
+	
+	Runnable r2 = () -> {
+		//central diagonal
+		for (int i = matrixSize+1; i < matrixSize*matrixSize-matrixSize-1; i++) {
+			for (int j = matrixSize+1; j <matrixSize*matrixSize-matrixSize-1; j++) {
+				if (i==j) {
+					someArray[i][j] = 8;
+					if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
+						someArray[i][j] = 1;
+					}
+					
+				}
+						
+			}
+			
+		}
+		
+		
+	};
+	
+	Thread t2 = new Thread(r2);
+	t2.start();
+	
+	Runnable r3 = () -> {
+		
+		//TopWidthDiagonal
+		
+		for (int i = matrixSize+1; i < matrixSize*matrixSize-matrixSize; i++) {
+			for (int j = matrixSize+1; j < matrixSize*matrixSize-matrixSize; j++) {
+				if (i + 1 == j) {
+					someArray[i][j] = 3;
+					if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
+						someArray[i][j] = 0;
+					}
+				}
+			}
+			
+		}
+	};
+
+	Thread t3 = new Thread(r3);
+	t3.start();
+	
+	
+	
+	Runnable r4 = () -> {
+		
+		//BottomWidthDiagonal
+		
+		for (int i = matrixSize; i < matrixSize*matrixSize-matrixSize-1; i++) {
+			for (int j = matrixSize; j < matrixSize*matrixSize-matrixSize-1; j++) {
+				if (j + 1 == i) {
+					someArray[i][j] = 3;
+					if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
+						someArray[i][j] = 0;
+					}
+				}
+			}
+			
+		}
+		
+	};
+	
+	Thread  t4 = new Thread(r4);
+	t4.start();
+	
+	
+	Runnable r5 = () -> {
+		//TopHeightDiagonal
+		for (int i = matrixSize+1; i < matrixSize*matrixSize-matrixSize+3; i++) {
+			for (int j = matrixSize+1; j < matrixSize*matrixSize-matrixSize+3; j++) {
+				if (i + matrixSize == j) {
+					someArray[i][j] = 5;
+					if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
+						someArray[i][j] = 0;
+					}
+
+				}
+			}
+			
+		}
+	};
+	
+	Thread t5 = new Thread(r5);
+	t5.start();
+	
+	
+	
+	
+	
 	
 	
 	}
