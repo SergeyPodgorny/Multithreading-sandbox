217a74fa6044c1576b893676180d1b91e8cb0919
SergeyPodgorny <serkonpod@gmail.com>
2022-03-17 15:13:06 +0300

    refactoring

--- src/com/multithreading/sandbox/thread/and/runnable/abstractFactory/SparseMatrix.java
+++ src/com/multithreading/sandbox/thread/and/runnable/abstractFactory/SparseMatrix.java
@@ -1,5 +1,26 @@
 package com.multithreading.sandbox.thread.and.runnable.abstractFactory;
 
-public class SparseMatrix {
+import static com.multithreading.sandbox.Sandbox.MATRIX_SIZE;
+
+public class SparseMatrix implements RunnableTask {
+	
+	
+	private volatile double [][] someArray = new double[MATRIX_SIZE*MATRIX_SIZE][MATRIX_SIZE*MATRIX_SIZE];
+
+	public SparseMatrix(double[][] someArray) {
+		this.someArray = someArray;
+	}
+	
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++) {
+			for (int j = 0; j < MATRIX_SIZE*MATRIX_SIZE; j++) {
+				if (i==j) {
+					someArray[i][j] = 1;
+				}
+			}
+		}
+	}
 
 }
