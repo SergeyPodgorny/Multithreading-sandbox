0fc631cbf43a79bf2df878053e04cfff39e9b0d0
SergeyPodgorny <serkonpod@gmail.com>
2022-03-17 15:09:13 +0300

    refactoring

--- src/com/multithreading/sandbox/thread/and/runnable/abstractFactory/PrimaryDiagonal.java
+++ src/com/multithreading/sandbox/thread/and/runnable/abstractFactory/PrimaryDiagonal.java
@@ -4,6 +4,15 @@ import static com.multithreading.sandbox.Sandbox.MATRIX_SIZE;
 
 public class PrimaryDiagonal implements RunnableTask{
 
+	
+	private double [][] someArray = new double[MATRIX_SIZE*MATRIX_SIZE][MATRIX_SIZE*MATRIX_SIZE];
+	
+	
+	public PrimaryDiagonal(double [][] someArray) {
+		this.someArray = someArray;
+	}
+	
+	
 	@Override
 	public void run() {
 		for (int i = MATRIX_SIZE+1; i < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE-1; i++) {
