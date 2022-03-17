b8ef675f8b21a5e2efa9b825bf6a00971e4cb5bd
SergeyPodgorny <serkonpod@gmail.com>
2022-03-17 12:17:17 +0300

    refactoring

--- src/com/multithreading/sandbox/thread/and/runnable/java1/benchmark/AnonymousClass.java
+++ src/com/multithreading/sandbox/thread/and/runnable/java1/benchmark/AnonymousClass.java
@@ -1,5 +1,7 @@
 package com.multithreading.sandbox.thread.and.runnable.java1.benchmark;
 
+import static com.multithreading.sandbox.Sandbox.MATRIX_SIZE;
+
 import java.util.Arrays;
 
 public class AnonymousClass extends Thread{
@@ -16,10 +18,10 @@ public class AnonymousClass extends Thread{
 		// creating sparse array
 		@Override
 		public void run() {
-			for (int i = 0; i < matrixSize*matrixSize; i++) {
-				for (int j = 0; j < matrixSize*matrixSize; j++) {
+			for (int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++) {
+				for (int j = 0; j < MATRIX_SIZE*MATRIX_SIZE; j++) {
 					if (i==j) {
-						someArray[i][j] = 2;
+						someArray[i][j] = 1;
 					}
 				}
 			}
@@ -34,11 +36,11 @@ public class AnonymousClass extends Thread{
 		// primary diagonal
 		@Override
 		public void run() {
-			for (int i = matrixSize+1; i < matrixSize*matrixSize-matrixSize-1; i++) {
-				for (int j = matrixSize+1; j <matrixSize*matrixSize-matrixSize-1; j++) {
+			for (int i = MATRIX_SIZE+1; i < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE-1; i++) {
+				for (int j = MATRIX_SIZE+1; j <MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE-1; j++) {
 					if (i==j) {
 						someArray[i][j] = 8;
-						if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
+						if ((i % MATRIX_SIZE == 3)^(i % MATRIX_SIZE == 0)) {
 							someArray[i][j] = 1;
 						}
 						
@@ -57,17 +59,17 @@ public class AnonymousClass extends Thread{
 		// TopWidthDiagonal
 		@Override
 		public void run() {
-			for (int i = matrixSize+1; i < matrixSize*matrixSize-matrixSize; i++) {
-				for (int j = matrixSize+1; j < matrixSize*matrixSize-matrixSize; j++) {
+			for (int i = MATRIX_SIZE+1; i < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE; i++) {
+				for (int j = MATRIX_SIZE+1; j < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE; j++) {
 					if (i + 1 == j) {
 						someArray[i][j] = 3;
-						if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
-							someArray[i][j] = 0;
+						if ((i % MATRIX_SIZE == 3)^(i % MATRIX_SIZE == 0)) {
+								someArray[i][j] = 0;
+							}
 						}
 					}
+					
 				}
-				
-			}
 	}
 		
 	}.start();
@@ -80,11 +82,11 @@ public class AnonymousClass extends Thread{
 		
 		@Override
 		public void run() {
-			for (int i = matrixSize; i < matrixSize*matrixSize-matrixSize-1; i++) {
-				for (int j = matrixSize; j < matrixSize*matrixSize-matrixSize-1; j++) {
+			for (int i = MATRIX_SIZE; i < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE-1; i++) {
+				for (int j = MATRIX_SIZE; j < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE-1; j++) {
 					if (j + 1 == i) {
 						someArray[i][j] = 3;
-						if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
+						if ((i % MATRIX_SIZE == 3)^(i % MATRIX_SIZE == 0)) {
 							someArray[i][j] = 0;
 						}
 					}
@@ -103,14 +105,13 @@ new Thread() {
 		
 		@Override
 		public void run() {
-			for (int i = matrixSize+1; i < matrixSize*matrixSize-matrixSize+3; i++) {
-				for (int j = matrixSize+1; j < matrixSize*matrixSize-matrixSize+3; j++) {
-					if (i + matrixSize == j) {
+			for (int i = MATRIX_SIZE+1; i < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE+3; i++) {
+				for (int j = MATRIX_SIZE+1; j < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE+3; j++) {
+					if (i + MATRIX_SIZE == j) {
 						someArray[i][j] = 5;
-						if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
+						if ((i % MATRIX_SIZE == 3)^(i % MATRIX_SIZE == 0)) {
 							someArray[i][j] = 0;
 						}
-
 					}
 				}
 				
@@ -131,11 +132,11 @@ new Thread() {
 		
 		@Override
 		public void run() {
-			for (int i = 1; i < matrixSize*matrixSize-matrixSize-1; i++) {
-				for (int j = 1; j < matrixSize*matrixSize-matrixSize-1; j++) {
-					if (j + matrixSize == i) {
+			for (int i = 1; i < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE-1; i++) {
+				for (int j = 1; j < MATRIX_SIZE*MATRIX_SIZE-MATRIX_SIZE-1; j++) {
+					if (j + MATRIX_SIZE == i) {
 						someArray[i][j] = 5;
-						if ((i % matrixSize == 3)^(i % matrixSize == 0)) {
+						if ((i % MATRIX_SIZE == 3)^(i % MATRIX_SIZE == 0)) {
 							someArray[i][j] = 0;
 						}
 					}
