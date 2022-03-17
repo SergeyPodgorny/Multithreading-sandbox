932002159488682b06a13dec0c81b9865072cdd7
SergeyPodgorny <serkonpod@gmail.com>
2022-03-17 15:21:02 +0300

    refactoring

--- src/com/multithreading/sandbox/thread/and/runnable/abstractFactory/MatrixFactory.java
+++ src/com/multithreading/sandbox/thread/and/runnable/abstractFactory/MatrixFactory.java
@@ -1,5 +1,12 @@
 package com.multithreading.sandbox.thread.and.runnable.abstractFactory;
 
+import static com.multithreading.sandbox.Sandbox.MATRIX_SIZE;
+
 public class MatrixFactory {
 
+	
+	private volatile double [][] someArray = new double[MATRIX_SIZE*MATRIX_SIZE][MATRIX_SIZE*MATRIX_SIZE];
+	
+	
+	
 }
