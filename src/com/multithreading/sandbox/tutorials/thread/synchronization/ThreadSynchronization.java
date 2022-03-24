143f0b5f04449928074d5957bc848049a3d45f5b
SergeyPodgorny <serkonpod@gmail.com>
2022-03-24 15:00:55 +0300

    synchronization problem solving

--- src/com/multithreading/sandbox/tutorials/thread/synchronization/ThreadSynchronization.java
+++ src/com/multithreading/sandbox/tutorials/thread/synchronization/ThreadSynchronization.java
@@ -3,22 +3,46 @@ package com.multithreading.sandbox.tutorials.thread.synchronization;
 public class ThreadSynchronization {
 
 	
-	public static void main(String args) {
+	/**
+	 * Здесь решается проблема синхронизации разных потоков, обращающихся к одному ресурсу
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
 		
 		
 		
 		
+		NonSyncData data1 = new NonSyncData(5);
 		
 		
-		
-		Runnable simpleWriting = () -> {
+		Runnable writer = () -> {
+			
+			for(int i = 0; i<5; i++) {
+				data1.setData(i);
+				System.out.println(Thread.currentThread().getName()+data1.getData());
+			}
 			
 		};
 		
-		Runnable simpleReading = () -> {
+		Runnable reader = () -> {
+			
+			
+			for(int i = 0; i <5; i++) {
+				System.out.println(Thread.currentThread().getName()+data1.getData());
+			}
 			
 		};
 		
+		
+		Thread writerThread = new Thread(writer);
+		
+		Thread readerThread = new Thread(reader);
+		
+		writerThread.start();
+		
+		readerThread.start();
 		
 	}
 	
