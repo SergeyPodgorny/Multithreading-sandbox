f64d94d9128fb10bad85313ce469ca33ea27a611
SergeyPodgorny <serkonpod@gmail.com>
2022-03-24 12:40:04 +0300

    thread lifecycle learning

--- src/com/multithreading/sandbox/tutorials/thread/life/cycle/Thread1.java
+++ src/com/multithreading/sandbox/tutorials/thread/life/cycle/Thread1.java
@@ -11,21 +11,31 @@ public class Thread1 extends Thread {
 	 * @throws InterruptedException
 	 */
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		
 		
 		
 		
 		Runnable r1 = () -> {
-			System.out.println("Not a main Thread");
+			System.out.println("Thread1");
 		};
 		
+		Runnable r2 = () -> {
+			System.out.println("Thread2");
+		};
+		
+		
 		Thread t1 = new Thread(r1);
+		t1.setPriority(MIN_PRIORITY);
 		t1.start();
 		
+		Thread t2 = new Thread(r2);
+		t2.setPriority(MAX_PRIORITY);
+		t2.start();
 		
-		Thread.yield();
+		
+		Thread.sleep(1000);
 		System.out.println("Thread main");
 		
 	
