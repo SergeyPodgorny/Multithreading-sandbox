b90473c34d02d8bed18550338650c5752cd70565
SergeyPodgorny <serkonpod@gmail.com>
2022-04-01 12:01:41 +0300

    volatile a better example

--- src/com/multithreading/sandbox/tutorials/thread/volatiles/VolatileTutorial.java
+++ src/com/multithreading/sandbox/tutorials/thread/volatiles/VolatileTutorial.java
@@ -3,45 +3,57 @@ package com.multithreading.sandbox.tutorials.thread.volatiles;
 public class VolatileTutorial {
 	
 	
-	static volatile char target;
+	static volatile int i;
 	
-	static int maxIter = 5;
 	
 	
 	public static void main (String[] args) {
 		
 		
+		Runnable writer = () -> {
+			
+			while(i < 5) {
+				
+				System.out.println("Incremented to " + ++i);
+				
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		
-		{
-		
-		Runnable writer = ()->{
-			
-			target= '{';
-//			System.out.println(target);
-			
-			
-			
-			target= '}';
-//			System.out.println(target+ " from " + Thread.currentThread().getName());
-			
-						
 		};
 		
 		
 		
-		new Thread(writer) {
+		Runnable reader = ()-> {
 			
+			int localI = i;
+				
+				while (localI<5) {
+					if (localI!=1) {
+						System.out.println("value of i is "+ i);
+						localI = i;
+					}
+					
+					
+				}
+			
+		};
+		
+		new Thread(writer) {
 		}.start();
 		
-		new Thread(writer) {
-			
+		
+		
+		new Thread(reader) {
 		}.start();
-		
-		
-		
-		}
-		
-		
 		
 	}
+	
+	
+	
+	
+	
 }
