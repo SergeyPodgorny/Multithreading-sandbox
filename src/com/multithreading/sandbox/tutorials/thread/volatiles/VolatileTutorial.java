1088e5b6945a516bd4c47c67ea3483d6ee116b9b
SergeyPodgorny <serkonpod@gmail.com>
2022-04-01 11:24:01 +0300

    volatile tutorial

--- src/com/multithreading/sandbox/tutorials/thread/volatiles/VolatileTutorial.java
+++ src/com/multithreading/sandbox/tutorials/thread/volatiles/VolatileTutorial.java
@@ -3,8 +3,47 @@ package com.multithreading.sandbox.tutorials.thread.volatiles;
 public class VolatileTutorial {
 	
 	
+	static volatile int target;
+	
+	static int maxIter = 5;
 	
 	
-	
-
+	public static void main (String[] args) {
+		
+		
+		
+		{
+		
+		Runnable writer = ()->{
+			
+			while(maxIter < 5) {
+				target++;
+			}
+			
+			
+		};
+		
+		Runnable printer = () -> {
+			
+			while (target > 1) {
+			
+				System.out.println(target);
+				
+			}
+			
+		};
+		
+		
+		new Thread(writer) {
+			
+		}.start();
+		
+		
+		new Thread (printer) {
+			
+		}.start();
+		
+		
+		}
+	}
 }
