a927e3b98bb951e83075c17955c3df394733d822
SergeyPodgorny <serkonpod@gmail.com>
2022-04-07 11:55:57 +0300

    done with volatile

--- src/com/multithreading/sandbox/tutorials/thread/volatiletutorial/AnotherJMM.java
+++ src/com/multithreading/sandbox/tutorials/thread/volatiletutorial/AnotherJMM.java
@@ -1,22 +1,47 @@
 package com.multithreading.sandbox.tutorials.thread.volatiletutorial;
 
+import static org.junit.jupiter.api.DynamicTest.stream;
+
 public class AnotherJMM {
 	
-	static int x;
-	static boolean isReady;
+	static volatile int x;
+	static volatile boolean isReady;
 	
 	
 	
 	public static void main(String[] args) {
 		
+		new Thread2().start();
 		
+		new Thread1().start();
 		
 		
 		
 		
 	}
 	
+	private static class Thread1 extends Thread{
+		
+		@Override
+		public void run() {
+			x = 1;
+			isReady = true;
+		}
+		
+		
+		
+	}
 	
+	private static class Thread2 extends Thread{
+		
+		@Override
+		public void run() {
+			if (isReady) {
+				System.out.println(x);
+			}
+		}
+		
+	}
 	
 	
 
