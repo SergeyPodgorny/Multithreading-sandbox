ac62c48e5eb0a24c1ffb2dea4f077add8f76ede2
SergeyPodgorny <serkonpod@gmail.com>
2022-04-07 12:18:02 +0300

    working with atomic numbers

--- src/com/multithreading/sandbox/tutorials/thread/atomic/AtomicNumbers.java
+++ src/com/multithreading/sandbox/tutorials/thread/atomic/AtomicNumbers.java
@@ -1,20 +1,30 @@
 package com.multithreading.sandbox.tutorials.thread.atomic;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AtomicNumbers {
 
 	static int i;
 	
+	static AtomicInteger I = new AtomicInteger(0);
 	
 	public static void main (String[] args) {
 		
-		for (int j =0; j< 10000; j++) {
+		for (int j =0; j< 1000; j++) {
 			new Thread1().start();
 		}
 		
+		System.out.println(Thread.currentThread().getName()+ " "+i);
 		
 		
 		
-		System.out.println(i);
+		for (int j =0; j< 1000; j++) {
+			new Thread2().start();
+		}
+		
+		System.out.println(Thread.currentThread().getName()+ " "+ I.get());
+		
+		
 		
 	}
 	
@@ -26,5 +36,17 @@ public class AtomicNumbers {
 		}
 		
 	}
+	
+	public static class Thread2 extends Thread{
+		
+		@Override
+		public void run() {
+			I.incrementAndGet();
+		}
+		
+		
+	}
+	
+	
 	
 }
