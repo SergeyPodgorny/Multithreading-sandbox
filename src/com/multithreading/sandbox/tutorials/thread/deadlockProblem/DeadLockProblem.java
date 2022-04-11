9b7e15d5139684ff77beb1b01e0209d5ec31bbef
SergeyPodgorny <serkonpod@gmail.com>
2022-04-11 11:48:43 +0300

    working on deadlock problem

--- src/com/multithreading/sandbox/tutorials/thread/deadlockProblem/DeadLockProblem.java
+++ src/com/multithreading/sandbox/tutorials/thread/deadlockProblem/DeadLockProblem.java
@@ -5,6 +5,22 @@ public class DeadLockProblem {
 	public static void main (String[] args) {
 		
 		
+		
+		Runnable r1 = () -> {
+			DangerousClass1 class1 = new DangerousClass1(); 
+		};
+		
+		Runnable r2 = () -> {
+			DangerousClass2 class1 = new DangerousClass2(); 
+		};
+		
+		new Thread(r1).start();
+		
+		new Thread(r2).start();
+		
+		
+		
+		
 	
 	}
 	
